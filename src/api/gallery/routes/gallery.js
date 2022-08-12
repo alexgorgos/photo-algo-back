52e5e6b20a629567f2b2dfcee76fe998d67416cc
Alexandru Gorgos <hello@alexandrugorgos.com>
2022-08-12 12:20:25 +0100

    changed category to gallery

--- src/api/gallery/routes/gallery.js
+++ src/api/gallery/routes/gallery.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * category router.
+ * gallery router.
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::category.category');
+module.exports = createCoreRouter('api::gallery.gallery');
