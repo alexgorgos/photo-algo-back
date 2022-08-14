3d5febd405de8b726e99199ece0d95642a8b381b
Alexandru Gorgos <hello@alexandrugorgos.com>
2022-08-14 17:39:05 +0100

    Update config.js

--- src/api/config/controllers/config.js
+++ src/api/config/controllers/config.js
@@ -1,9 +1,20 @@
-'use strict';
+"use strict";
 
 /**
  *  config controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::config.config');
+module.exports = createCoreController("api::config.config", ({ strapi }) => ({
+  async find(ctx) {
+    const { query } = ctx;
+
+    const entity = await strapi.entityService.find({
+      ...query,
+      populate: "deep",
+    });
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+    return this.transformResponse(sanitizedEntity);
+  },
+}));
