83d7f211a5b0e043415d2fd4bc924b249737af5f
Alexandru Gorgos <hello@alexandrugorgos.com>
2022-08-14 19:52:53 +0100

    deep query fix

--- src/api/gallery/controllers/gallery.js
+++ src/api/gallery/controllers/gallery.js
@@ -1,9 +1,23 @@
-'use strict';
+"use strict";
 
 /**
  *  gallery controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::gallery.gallery');
+module.exports = createCoreController("api::gallery.gallery", ({ strapi }) => ({
+  async find(ctx) {
+    ctx.query = { ...ctx.query, populate: "deep" };
+
+    const { data, meta } = await super.find(ctx);
+    return { data, meta };
+  },
+
+  async findOne(ctx) {
+    ctx.query = { ...ctx.query, populate: "deep" };
+
+    const { data, meta } = await super.find(ctx);
+    return { data, meta };
+  },
+}));
